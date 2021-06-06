fd855ac16825b8a3fbc76dbf5a773a44b3ef6297
Hugo <hugolacerdar@gmail.com>
2021-06-06 19:56:55 -0300

    Adding user table migration

---
+++ priv/repo/migrations/20210606201720_create_user_table.exs
@@ -0,0 +1,11 @@
+defmodule Gitrepos.Repo.Migrations.CreateUserTable do
+  use Ecto.Migration
+
+  def change do
+    create table :users do
+      add :password_hash, :string
+
+      timestamps()
+    end
+  end
+end
