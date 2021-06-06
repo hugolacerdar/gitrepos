1bccb3dbc4dc6f2ea93a2fa462c61d13594c4d08
Hugo <hugolacerdar@gmail.com>
2021-06-06 19:58:58 -0300

    Adding auth pipeline

--- lib/gitrepos_web/router.ex
+++ lib/gitrepos_web/router.ex
@@ -1,12 +1,26 @@
 defmodule GitreposWeb.Router do
   use GitreposWeb, :router
+  alias GitreposWeb.Plugs.UUIDChecker
 
   pipeline :api do
     plug :accepts, ["json"]
+    plug UUIDChecker
+  end
+
+  pipeline :auth do
+    plug GitreposWeb.Auth.Pipeline
+    plug UUIDChecker
   end
 
   scope "/api", GitreposWeb do
     pipe_through :api
+
+    post "/users", UsersController, :create
+    post "/users/signin", UsersController, :sign_in
+  end
+
+  scope "/api", GitreposWeb do
+    pipe_through :auth
 
     get "/repos/:username", ReposController, :get_all
   end
