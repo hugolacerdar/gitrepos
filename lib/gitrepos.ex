b1bd28f2b3f83650438a98ccef06ca85c74550b5
Hugo <hugolacerdar@gmail.com>
2021-06-06 19:59:33 -0300

    Adding users facades

--- lib/gitrepos.ex
+++ lib/gitrepos.ex
@@ -1,5 +1,9 @@
 defmodule Gitrepos do
   alias Gitrepos.GithubPublicApi.Client
+  alias Gitrepos.Users.Create, as: CreateUser
+  alias Gitrepos.Users.Get, as: GetUser
 
   defdelegate get_filtered_repo_data(username), to: Client, as: :get_repos_by_username
+  defdelegate create_user(params), to: CreateUser, as: :call
+  defdelegate get_user_by_id(id), to: GetUser, as: :by_id
 end
