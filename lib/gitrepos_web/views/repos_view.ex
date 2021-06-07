101fae268785bf267b781cc25a7aa033ee80b621
Hugo <hugolacerdar@gmail.com>
2021-06-06 21:16:39 -0300

    Adding refresh token logic

--- lib/gitrepos_web/views/repos_view.ex
+++ lib/gitrepos_web/views/repos_view.ex
@@ -1,5 +1,6 @@
 defmodule GitreposWeb.ReposView do
   use GitreposWeb, :view
 
-  def render("show.json", %{repositories: repositories}), do: %{result: repositories}
+  def render("show.json", %{repositories: repositories, refresh_token: refresh_token}),
+    do: %{result: repositories, refresh_token: refresh_token}
 end
